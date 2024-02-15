1a36e46001d6acbd1238e1438fa160aeca42a96d
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-15 10:44:16 +0100

    Share Logo

--- src/components/movies/components/Header.tsx
+++ src/components/movies/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { IconMovie } from "@tabler/icons-react";
 
+import { Logo } from "./Shared";
 import { Search } from "./Search";
 
 interface HeaderProps {
@@ -11,10 +11,9 @@ export const Header: React.FC<HeaderProps> = ({ onSearch }) => {
   return (
     <div className="flex w-full justify-center bg-slate-900">
       <nav className="flex w-full max-w-screen-lg flex-wrap items-center justify-center gap-5 p-5 sm:justify-between">
-        <h1 className="primary flex items-center justify-center space-x-3 font-medium">
-          <IconMovie />
-          <span>MovieDB</span>
-        </h1>
+        <div className="primary font-medium">
+          <Logo />
+        </div>
         <Search onSubmit={onSearch} />
       </nav>
     </div>
---
+++ src/components/movies/components/Shared.tsx
@@ -0,0 +1,8 @@
+import { IconMovie } from "@tabler/icons-react";
+
+export const Logo = () => (
+  <h1 className="flex items-center justify-center space-x-2">
+    <IconMovie />
+    <span>MovieDB</span>
+  </h1>
+);
